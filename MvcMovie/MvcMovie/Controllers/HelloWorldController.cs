ed69af5f32de2fa18a975af95e7cf9693f2a8d1a
GonFrecks1 <valenzuela.gracian@gmail.com>
2021-09-28 19:47:57 -0700

    add MvcMovie Project first time

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            //string res = HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            //return res;
+
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+
+        }
+    }
+}
